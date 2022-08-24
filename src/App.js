5337e627330005b03b4231e65c68066c831dec0d
Rajshree Jajoo <rajshreejajoo77@gmail.com>
2022-08-24 22:40:17 +0530

    shoppingsite

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import AppBar from "@material-ui/core/AppBar";
+import Box from "@material-ui/core/Box";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import Shoppingpage from "./Shoppingpage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <AppBar
+        style={{
+          backgroundColor: "rgb(242, 222, 247)",
+          textAlign: "center",
+          marginTop: "10px",
+        }}
+        position="static"
+      >
+        <Typography variant="h3" component="div">
+          Redux Shopping Site
+        </Typography>
+      </AppBar>
+      {/* <div className="App"> */}
+      {/* <header className="App-header"> */}
+      <Shoppingpage />
+      {/* </header> */}
+      {/* </div> */}
+    </Container>
   );
 }
 
---
+++ src/Shoppingpage.js
@@ -0,0 +1,43 @@
+import AppBar from "@material-ui/core/AppBar";
+import Box from "@material-ui/core/Box";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+
+const Shoppingpage = () => {
+  return (
+    <>
+      <Container>
+        <h1> Shopping Page</h1>
+        <Card style={{ width: "25%", backgroundColor: "lightgoldenrodyellow" }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles....
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Container>
+    </>
+  );
+};
+
+export default Shoppingpage;
