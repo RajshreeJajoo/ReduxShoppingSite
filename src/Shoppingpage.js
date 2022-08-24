6aad6870f60dbb7d827c90c3e587c20d7884c66e
Rajshree Jajoo <rajshreejajoo77@gmail.com>
2022-08-24 23:44:11 +0530

    site

--- src/Shoppingpage.js
+++ src/Shoppingpage.js
@@ -4,30 +4,45 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
-
+import axios from "axios";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
+import { useState } from "react";
 
 const Shoppingpage = () => {
+  const [price, setPrice] = useState();
+  const [category,setCategory] = useState();
+  const showShopping = () => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        setPrice(res.data[0].price);
+        setCategory(res.data[0].category);
+      })
+      .catch((err) => {
+        console.log(err);
+      }); 
+  };
   return (
     <>
       <Container>
         <h1> Shopping Page</h1>
+        <Button onClick={showShopping}>Click for Shoopping Items</Button>
         <Card style={{ width: "25%", backgroundColor: "lightgoldenrodyellow" }}>
-          <CardMedia
+          {/* <CardMedia
             component="img"
             height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
+            image={setData(alldata.image)}
             alt="green iguana"
-          />
+          /> */}
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-              Lizard
+            {category}
             </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles....
+            <Typography variant="body2">
+            {price}
             </Typography>
           </CardContent>
           <CardActions>
